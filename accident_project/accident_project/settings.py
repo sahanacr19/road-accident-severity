c87dccc8d44a8529f05cafc622b030f0e28d1d4f
sahanacr <sahanasahana93538@gmail.com>
2026-03-29 19:36:46 +0530

    Fix allowed hosts

--- accident_project/accident_project/settings.py
+++ accident_project/accident_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v2d#0)mf$^qrx8agn4!2cexjv2t#v46i69ny!ssn3j_rnbz5mv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
